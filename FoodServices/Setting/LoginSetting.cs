984984811da47106a2ed409efea57456498dd9c6
mhmdwalibaba <mhmdlybaba191@gmail.com>
2025-08-26 11:20:11 +0330

    add setting folder and loginsetting model for appsetting json

---
+++ FoodServices/Setting/LoginSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodServices.Setting
+{
+	public class LoginSetting
+	{
+		public string UserName {  get; set; }
+		public string Password { get; set; }
+		public string Url {  get; set; }
+	}
+}
