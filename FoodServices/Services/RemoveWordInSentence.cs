a85bbf342928bd3fc3045d1ed55b9a6c3d0294ff
mhmdwalibaba <mhmdlybaba191@gmail.com>
2025-09-02 12:51:26 +0330

    add extention for remove word in sentence (remove iptv word in menu.Name)

---
+++ FoodServices/Services/RemoveWordInSentence.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FoodServices.Services
+{
+	public static class RemoveWordInSentence
+	{
+		// Extension method on string
+		public static string RemoveWord(this string sentence, string word)
+		{
+			if (string.IsNullOrWhiteSpace(sentence) || string.IsNullOrWhiteSpace(word))
+				return sentence;
+
+			string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+			string result = string.Join(" ",
+				words.Where(w => !w.Equals(word, StringComparison.OrdinalIgnoreCase)));
+
+			return result;
+		}
+	}
+}
